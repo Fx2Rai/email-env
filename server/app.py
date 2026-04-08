8692997082d9904046cc6b043958bff6d7e3b013
Fx2Rai <fabianbiju7@gmail.com>
2026-04-08 20:09:22 +0530

    final fix

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from server.email_env_environment import EmailEnv
+from models import EmailAction
 
 app = FastAPI()
 env = EmailEnv()
@@ -9,15 +10,19 @@ def reset():
     obs = env.reset()
     return obs.dict()
 
-from models import EmailAction
-
 @app.post("/step")
 def step(action: EmailAction):
     obs, reward, done, info = env.step(action)
-
     return {
         "observation": obs.dict(),
         "reward": reward,
         "done": done,
         "info": info
     }
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    main()
